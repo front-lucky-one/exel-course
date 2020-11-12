595568ccd585437e512e93888be9bbb5ec3cd723
Vlad <onvladik7@gmail.com>
2020-11-12 03:18:49 +0100

    первый коммит

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,91 @@
-// с помощью require мы подключаем плагин в нод жс
 const path = require('path')
-const { resolve } = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const loader = require('sass-loader');
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
-const jsLoader = () => {
-    const loaders = [{
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
 
-    ]
-    if (isDev) {
-        loaders.push('eslint-loader')
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
     }
-    return loaders
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
 }
-console.log('Prod', isProd)
-console.log('dev', isDev)
+
 module.exports = {
-    // сперва указываем где лежат осходники приложения
-    // вызываем метод резолв у найденого плагина 
-    // и соединяем системную переменную __dirname(она указывает на абсолютный путь к папке)
-    // и src
-    context: path.resolve(__dirname, 'src'),
-    // моде указывает на режим разработки
-    mode: 'development',
-    // входные точки для приложения
-    entry: "./index.js",
-    // output это обьект настроек для конечного вывода js
-    output: {
-        // имя конечного файла с скриптами
-        filename: filename('js'),
-        // путь куда будут выгражаться конечные файлы js
-        path: resolve(__dirname, 'dist')
-
-
-    },
-    //
-    resolve: {
-        // по умолчанию загрузка js
-        extensions: ['.js'],
-        // алиас это символ который сокращает путь 
-        alias: {
-            "@": path.resolve(__dirname, 'src'),
-            "@core": path.resolve(__dirname, 'src/core')
-        }
-    },
-    devtool: isDev ? "source-map" : false,
-    devServer: {
-        hot: isDev,
-        port: 9000,
-        contentBase: path.join(__dirname, 'dist')
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({
-            template: 'index.html',
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
             }
-        }),
-        new MiniCssExtractPlugin({
-            filename: filename('css')
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist') },
-
-            ],
-        }),
-    ],
-    module: {
-        // описывает правила для лоадеров
-        rules: [{
-                // расширение sass или сss или scss
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: jsLoader(),
-            }
+          },
+          'css-loader',
+          'sass-loader'
         ],
-    },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      }
+    ]
+  }
 }
